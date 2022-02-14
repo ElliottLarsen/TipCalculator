980c391a84e0c219295892c67df78ca8affc0043
Elliott Larsen <larsenel@oregonstate.edu>
2022-02-14 00:01:42 -0600

    Initial commit.

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Elliott Larsen on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
