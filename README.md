de103e8b4fb21cfd433415e0e2fc889e205c5fb0
Elliott Larsen <larsenel@oregonstate.edu>
2022-02-15 00:12:23 -0600

    Project complete.  README added.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tip Calculator
+
+This is an iPhone application that calculates gratuity.  The application has various functionalities that allow users to select a desired percentage and split totals.  UI components include: application icon, application launch screen, calculate button.
+
+<image src = "http://g.recordit.co/gKmdgdweZk.gif" width = 250><br>
+
+<br>
+<h3 align= "right"> Elliott Larsen </h3>
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -13,27 +13,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
-    
-    
+    @IBOutlet weak var splitByField: UITextField!
+    @IBOutlet weak var perPersonLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     
-    @IBAction func calculateTip(_ sender: Any) {
+    //@IBAction func calculateTip(_ sender: Any) {
         
+        //let bill = Double(billAmountField.text!) ?? 0
+        
+        // Get Total tip by multiplying tip * tipPercentage
+        //let tipPercentages = [0.15, 0.18, 0.20]
+        //let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        //let total = bill + tip
+        
+        // Split the bill
+        //let personCount = Int(splitByField.text!) ?? 0
+        //let perPerson = Double(total) / Double(personCount)
+        
+    //}
+    
+    @IBAction func calculateButton(_ sender: Any) {
         let bill = Double(billAmountField.text!) ?? 0
-        
         let tipPercentages = [0.15, 0.18, 0.20]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
-        
+        let personCount = Int(splitByField.text!) ?? 0
+        let perPerson = Double(total) / Double(personCount)
+    
+        // Update Tip Amount Label
         tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
         totalLabel.text = String(format: "$%.2f", total)
-        
+        // Update the per person bill amount
+        perPersonLabel.text = String(format: "$%.2f", perPerson)
     }
+    
+
     
 }
 
