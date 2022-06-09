efc32172c5d85b5b39986c80d9baccdb1634226a
Elliott Larsen <elliottlrsn@gmail.com>
2022-06-08 21:34:46 -0500

    Display works correctly.  Constraints for the launch screen and rounding error message.

--- Tip Calculator/ViewControllers/MainViewController.swift
+++ Tip Calculator/ViewControllers/MainViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
-extension NSString  {
-    var isNumber: Bool {
-        return length > 0 && rangeOfCharacter(from: CharacterSet.decimalDigits.inverted).location == NSNotFound
+extension String {
+    var isNum: Bool {
+        if (Int(self) != nil) || (Float(self) != nil){
+            return true
+        } else {
+            return false
+        }
     }
 }
 
@@ -25,53 +29,71 @@ class MainViewController: UIViewController {
     @IBOutlet weak var tipPercentage: UISlider!
     @IBOutlet weak var splitByLabel: UILabel!
     @IBOutlet weak var splitBy: UISlider!
-    
+    @IBOutlet weak var currency: UILabel!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-    }
-    @IBAction func tipPercentChanged(_ sender: UISlider) {
-        let percent = String(format: "%.0f", sender.value)
-        tipPercentLabel.text = "\(percent)%"
-        //print(percent)
-    }
-    
-    @IBAction func spliByChanged(_ sender: UISlider) {
-        let people = String(format: "%.0f", sender.value)
-        splitByLabel.text = "\(people) People"
-        //print(people)
-    }
-    
-    @IBAction func calculateButton(_ sender: UIButton) {
-        
-        let numOfPeople = String(format: "%.0f", splitBy.value)
-        let tipPercent = String(format: "%.0f", tipPercentage.value)
-        let people = Int(numOfPeople)
-        let percent = Float(tipPercent)! * 0.01
-        
-        
-        
-        let billAmount = Float(billAmountTextField.text!)
-        let workingTipAmount = billAmount! * percent
-        let totalAmount = billAmount! + workingTipAmount
-        let tipAmount = String(format: "%.2f", workingTipAmount)
-        let perPerson = String(format: "%.2f", totalAmount / Float(people!))
-       
-        
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+          
 
-        
-        
-        print(billAmount!)
-        print(tipAmount)
-        print(totalAmount)
-        print(perPerson)
-        //tipAmount = billAmount * (Int(tipPercentage.value)/10)
-        
-        performSegue(withIdentifier: "toResult", sender: self)
-
-    }
+        }
     
-}
+        @IBAction func tipPercentChanged(_ sender: UISlider) {
+            let percent = String(format: "%.0f", sender.value)
+            tipPercentLabel.text = "\(percent)%"
+            //print(percent)
+        }
+        
+        @IBAction func spliByChanged(_ sender: UISlider) {
+            let people = String(format: "%.0f", sender.value)
+            if (people == "1"){
+                splitByLabel.text = "\(people) Person"
+            } else {
+                splitByLabel.text = "\(people) People"
+            }
+                    
+
+            //print(people)
+        }
+        
+        @IBAction func calculateButton(_ sender: UIButton) {
+            
+            let numOfPeople = String(format: "%.0f", splitBy.value)
+            let tipPercent = String(format: "%.0f", tipPercentage.value)
+            let people = Int(numOfPeople)
+            let percent = Float(tipPercent)! * 0.01
+            
+            
+            let billEntered = billAmountTextField.text!
+            if billEntered.isNum {
+                let billAmount = Float(billAmountTextField.text!)
+                displayBillAmount = billAmountTextField.text!
+                let workingTipAmount = billAmount! * percent
+                let totalAmount = billAmount! + workingTipAmount
+                displayTotalAmount = String(format: "%.2f", billAmount! + workingTipAmount)
+                displayTipAmount = String(format: "%.2f", workingTipAmount)
+                displayPerPerson = String(format: "%.2f", totalAmount / Float(people!))
+
+                performSegue(withIdentifier: "toResult", sender: self)
+            } else{
+                currency.text = "Please enter a number."
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5) {
+                    self.currency.text = "USD"
+                }
+            }
+        }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "toResult" {
+                let destinationVC = segue.destination as! ResultsViewController
+                destinationVC.billAmount = displayBillAmount
+                destinationVC.tipAmount = displayTipAmount
+                destinationVC.totalAmount = displayTotalAmount
+                destinationVC.perPerson = displayPerPerson
+                destinationVC.splitBy = String(format: "%.0f", splitBy.value)
+            }
+        }
+        
+    }
+
 
--- Tip Calculator/ViewControllers/ResultsViewController.swift
+++ Tip Calculator/ViewControllers/ResultsViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    var billAmount: String?
+    var tipAmount: String?
+    var totalAmount: String?
+    var perPerson: String?
+    var splitBy: String?
+    
+    var people: Int?
+    var billAmountNum: Float?
+    
+    @IBOutlet weak var billAmountLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var perPersonLabel: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        billAmountLabel.text = "$\(billAmount ?? "0.00")"
+        tipAmountLabel.text = "$\(tipAmount ?? "0.00")"
+        totalAmountLabel.text = "$\(totalAmount ?? "0.00")"
+        perPersonLabel.text = "$\(perPerson ?? "0.00")"
+    
+     }
+     
+    @IBAction func recalculateButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
     }
     
+    
 
-    /*
-    // MARK: - Navigation
+     /*
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
 
-}
+ }
